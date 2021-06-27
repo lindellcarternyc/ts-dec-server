ece5cff405b92255c2a37e29c35f1de1e2bef08a
Lindell Carter <lindellcarternyc@gmail.com>
2021-06-27 18:00:21 -0400

    uses new Application module

--- src/example.ts
+++ src/example.ts
@@ -1,5 +1,9 @@
+import { Application, EchoServer, PlatformExpress } from './application'
+
 const main = async () => {
-  console.log('Hello World!')
+  const app: Application = await PlatformExpress.bootstrap(EchoServer)
+  await app.listen()
+  console.log(`App is listening on port=${app.port}`)
 }
 
 main()
